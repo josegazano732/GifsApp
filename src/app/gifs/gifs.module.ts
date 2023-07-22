88f71cfd5fcb06adfca927b6a34e98b15d617f9d
josegazano732 <josegazano7@outlook.com>
2023-07-22 16:45:18 -0300

    Avances en modulo gifs

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.componets';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HomePageComponent
+    
   ]
 })
 export class GifsModule { }
