ce0c761cce15d83efb2c317c5e47319b43248ca8
josegazano732 <josegazano7@outlook.com>
2023-08-19 16:16:13 -0300

    Se agrego consulta de APi en el service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { promises } from 'dns';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
     private _history: string[]=[];
+
+    private apiKey:string='lU4Otdgj2booptknVCgbaT4qioEFT1Yi';
 
     constructor() { }
     
@@ -20,12 +23,14 @@ export class GifsService {
         this._history.unshift(tag);
         this._history= this._history.splice(0,10);
     }
-
-    searchTag(tag:string):void {
+// Esta es una forma de realizarlo con javascript una peticion API
+    async searchTag(tag:string):Promise <void> {
         if (tag.length===0) return; // Si el tag o el input es igual a 0 no retorna nada.
         this.organizeHistory(tag);
-       
         //console.log(this._history);
         
+        fetch('https://api.giphy.com/v1/gifs/search?api_key=lU4Otdgj2booptknVCgbaT4qioEFT1Yi&q=valorant&limit=10')
+            .then( res=> res.json())
+            .then( data=> console.log(data))
     }
 }
