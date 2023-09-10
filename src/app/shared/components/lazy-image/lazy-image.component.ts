de3a131860eabb7be0d6359b832c575027d1469e
josegazano732 <josegazano7@outlook.com>
2023-09-10 13:59:51 -0300

    JLG

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -22,8 +22,11 @@ export class LazyImageComponent implements OnInit {
   }
 
   onLoad(){
-    console.log('Image Loaded')
-    this.hasLoaded=true;
+    setTimeout(() => {
+      this.hasLoaded=true;
+    }, 1000);
+
+    
   }
 
 }
